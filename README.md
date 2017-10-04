9024d8e30156ca61b9eed9f5669a56a12f0fa10e
JiahuiRuan <jr58@rice.edu>
2017-10-04 16:14:06 -0500

    finish backend

---
+++ README.md
@@ -0,0 +1,2 @@
+# cpuReporter
+This is the repo of cpuReporter which aims at monitoring the cpu load
---
+++ backend/src/cpuload.js
@@ -0,0 +1,39 @@
+const os = require('os')
+const windowsCPU = require('windows-cpu')
+
+const getLoad = (req, res) => {
+  if (os.type() == 'Windows_NT') {
+    // Windows platform
+    windowsCPU.totalLoad((err, loadArr) => {
+      if (err) {
+        res.status(500).send({error: "something went wrong in windows"})
+      } else {
+        // loadArr (multi-cpu in percent) => [3, 10]
+        const sumLoad = loadArr.reduce((sum, value) => {
+          return sum + value
+        }, 0)
+        // change to percentage
+        sumLoad /= 100
+        const avgLoad = sumLoad / loadArr.length
+        res.status(200).send({
+          avgLoad: avgLoad,
+          sumLoad: sumLoad,
+          cpus: loadArr.length
+        })
+      }
+    })
+  } else {
+    // UNIX platform
+    res.status(200).send({
+      avgLoad: os.loadavg()[0] / os.cpus().length,
+      sumLoad: os.loadavg()[0],
+      cpus: os.cpus().reduce((arr, obj) => {
+        return arr.concat(obj.model)
+      }, [])
+    })
+  }
+}
+
+module.exports = (app) => {
+  app.get('/load', getLoad)
+}
